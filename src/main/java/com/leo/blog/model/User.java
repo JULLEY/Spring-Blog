7ed40972b59315a9cf830bcb417d2785f0cf65a3
woong <ung91@yahoo.com>
2020-11-09 00:04:49 +0900

    Board, Reply Model 생성

--- src/main/java/com/leo/blog/model/User.java
+++ src/main/java/com/leo/blog/model/User.java
@@ -1,5 +1,9 @@
 package com.leo.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -7,12 +11,16 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 // ORM -> OBJECT -> 테이블로 매핑해주는 기술
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder    // 빌더패턴
 @Entity // User 클래스가 MySql에 테이블 생성된다.
 public class User {
 
     @Id // primary key 선언
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 플젝에서 연결된 DB 넘버링 방식을 따라간다.
-    private long id; // auto_increment , sequence
+    private int id; // auto_increment , sequence
 
     @Column(nullable = false, length = 30)
     private String username; // 아이디
