fde3136e50b6ea879cd287a300f3c118001c182c
woong <ung91@yahoo.com>
2021-01-01 21:15:21 +0900

    시큐리티 적용중

--- src/main/java/com/leo/blog/config/auth/PrincipalDetail.java
+++ src/main/java/com/leo/blog/config/auth/PrincipalDetail.java
@@ -1,2 +1,48 @@
-package com.leo.blog.config.auth;public class PrincipalDetail {
+package com.leo.blog.config.auth;
+
+import com.leo.blog.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 오브젝트를
+// 스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+public class PrincipalDetail implements UserDetails {
+    private User user;  // 콤포지션(extends와 다름)
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 }
