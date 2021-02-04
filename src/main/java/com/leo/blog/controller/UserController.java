bf28a0abfda6fd7207c55aaf866349c2c1233392
woong <ung91@yahoo.com>
2021-02-05 00:14:04 +0900

    주석변경

--- src/main/java/com/leo/blog/controller/UserController.java
+++ src/main/java/com/leo/blog/controller/UserController.java
@@ -3,9 +3,9 @@ package com.leo.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// 인증이 안된 사용자들이 출입 할 수 있는 경로를 /auth/** 허용
+// 인증이 안된 사용자들이 출입 할 수 있는 경로를 /auth/** 허용한다
 // 그냥 주소가 / 이면 index.jsp 허용
-// static이하에 있는 /js/**, /css/**, /image/** 허용
+// static이하에 있는 /js/**, /css/**, /image/** 허용한다
 @Controller
 public class UserController {
 
